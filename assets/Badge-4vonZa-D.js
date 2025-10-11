7ee6198426218c6113953a6ce727ae30efe8655b
deniz-blue <deniz-blue@users.noreply.github.com>
2025-10-11 16:38:50 +0000

    deploy: 29b50b698227d770341251c11d9e051efaa8e4b0

--- assets/Badge-4vonZa-D.js
+++
@@ -1,1 +0,0 @@
-import{w as o,p as t}from"./chunk-B7RQU5TL-C1XVDo_W.js";import{S as r}from"./Stack-CzVLQlP4.js";import{T as e}from"./Text-CMBr5ycB.js";import"./factory-BEVx6DxP.js";import"./polymorphic-factory-DX7WSZXc.js";const f=o(function(){return t.jsx(r,{align:"center",justify:"center",w:"100vw",h:"100vh",children:t.jsx(e,{fz:"xs",fw:"bold",c:"white"})})});export{f as default};
--- assets/use-isomorphic-effect-yZOP3Ymm.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./chunk-B7RQU5TL-C1XVDo_W.js";const o=typeof document<"u"?e.useLayoutEffect:e.useEffect;export{o as u};
